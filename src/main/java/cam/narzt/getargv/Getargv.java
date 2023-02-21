6f33cae25cd6816e645c7fe2245e80d71e923dd1
Camden Narzt <c.narzt@me.com>
2023-02-21 11:31:44 -0700

    more docs, fix main class issues

--- src/main/java/cam/narzt/getargv/Getargv.java
+++ src/main/java/cam/narzt/getargv/Getargv.java
@@ -5,10 +5,23 @@ import java.nio.charset.Charset;
 import java.util.stream.Stream;
 import java.lang.module.ModuleDescriptor.Version;
 
-public class Getargv {
+/**
+ * Class to access the various functions of libgetargv, all methods/constants
+ * are static, so there's no reason to instantiate an instance of this class.
+ */
+public final class Getargv {
 
+    /**
+     * This is only here so you cannot instantiate this class
+     */
+    private Getargv() {
+        throw new AssertionError();
+    }
+
+    /** The maximum size of the arguments to a process on macOS. */
     static final int ARG_MAX = 1024 * 1024;
 
+    /** The maximum pid value on macOS. */
     static final int PID_MAX = Version.parse(System.getProperty("os.version")).compareTo(Version.parse("10.5")) < 0
             ? 30000
             : 99999;
@@ -22,19 +35,85 @@ public class Getargv {
     }
 
     // --- Native methods
-    private static native byte[][] get_argv_and_argc_of_pid(long pid);
+    /**
+     * native binding to get_argv_and_argc_of_pid in libgetargv
+     *
+     * @param pid the pid to target
+     * @return array of byte arrays representing the arguments
+     * @since 0.1
+     * @throws IOException when the underlying library call fails
+     *                     (permission/nonexistance/malformed).
+     */
+    private static native byte[][] get_argv_and_argc_of_pid(long pid) throws IOException;;
 
-    private static native byte[] get_argv_of_pid(long pid, long skip, boolean nuls);
+    /**
+     * native binding to get_argv_of_pid in libgetargv
+     *
+     * @param pid  the pid to target
+     * @param skip number of leading args to skip
+     * @param nuls replace nuls with spaces
+     * @return byte array of the arguments
+     * @since 0.1
+     * @throws IOException when the underlying library call fails
+     *                     (permission/nonexistance/malformed).
+     */
+    private static native byte[] get_argv_of_pid(long pid, long skip, boolean nuls) throws IOException;
 
     // -- Java methods
+    /**
+     * get arguments of pid as a byte array
+     *
+     * @param pid the process id (pid) of the process from which you want the
+     *            arguments
+     * @return the arguments of `pid` as a byte[], suitable for writing to
+     *         stdout/stderr/etc.
+     * @throws IOException when the underlying library call fails
+     *                     (permission/nonexistance/malformed).
+     * @since 0.1
+     * @see <a href=
+     *      "https://docs.oracle.com/en/java/javase/19/docs/specs/jni/types.html#modified-utf-8-strings">Discussion
+     *      of Java string encoding</a>
+     */
     public static byte[] asBytes(long pid) throws IOException {
         return asBytes(pid, 0);
     }
 
+    /**
+     * get arguments of pid as a byte array
+     *
+     * @param pid  the process id (pid) of the process from which you want the
+     *             arguments
+     * @param skip the number of leading arguments to skip over (leave off)
+     * @return the arguments of `pid` as a byte[], suitable for writing to
+     *         stdout/stderr/etc.
+     * @throws IOException when the underlying library call fails
+     *                     (permission/nonexistance/malformed).
+     * @since 0.1
+     * @see <a href=
+     *      "https://docs.oracle.com/en/java/javase/19/docs/specs/jni/types.html#modified-utf-8-strings">Discussion
+     *      of Java string encoding</a>
+     */
     public static byte[] asBytes(long pid, long skip) throws IOException {
         return asBytes(pid, skip, false);
     }
 
+    /**
+     * get arguments of pid as a byte array
+     *
+     * @param pid  the process id (pid) of the process from which you want the
+     *             arguments
+     * @param skip the number of leading arguments to skip over (leave off)
+     * @param nuls whether to replace the nul delimiters with spaces for human
+     *             reading
+     * @return the arguments of `pid` as a byte[], suitable for writing to
+     *         stdout/stderr/etc.
+     * @throws IOException when the underlying library call fails
+     *                     (permission/nonexistance/malformed).
+     * @since 0.1
+     * @see <a href=
+     *      "https://docs.oracle.com/en/java/javase/19/docs/specs/jni/types.html#modified-utf-8-strings">Discussion
+     *      of Java string encoding</a>
+     */
     public static byte[] asBytes(long pid, long skip, boolean nuls) throws IOException {
         if (skip < 0 || skip > ARG_MAX) {
             throw new IllegalArgumentException("skip outside valid range: 0-" + ARG_MAX);
@@ -45,6 +124,19 @@ public class Getargv {
         return get_argv_of_pid(pid, skip, nuls);
     }
 
+    /**
+     * Get the args of `pid` as an array of byte[] that you can inspect.
+     *
+     * @param pid the process id (pid) of the process from which you want the
+     *            arguments
+     * @return the arguments of `pid` as an array of byte[], suitable for inspection
+     * @throws IOException when the underlying library call fails
+     *                     (permission/nonexistance/malformed).
+     * @since 0.1
+     * @see <a href=
+     *      "https://docs.oracle.com/en/java/javase/19/docs/specs/jni/types.html#modified-utf-8-strings">Discussion
+     *      of Java string encoding</a>
+     */
     public static byte[][] asArray(long pid) throws IOException {
         if (pid < 0 || pid > PID_MAX) {
             throw new IllegalArgumentException("pid outside valid range: 0-" + PID_MAX);
@@ -52,16 +144,68 @@ public class Getargv {
         return get_argv_and_argc_of_pid(pid);
     }
 
-    // strings are tricky, must deal with encoding:
-    // https://docs.oracle.com/en/java/javase/11/docs/specs/jni/types.html
+    /**
+     * Get the args of `pid` as a String, note the OS doesn't enforce an encoding on
+     * arguments to a process, so there is no guarantee that any particular Charset
+     * will be valid.
+     *
+     * @param pid     the process id (pid) of the process from which you want the
+     *                arguments
+     * @param charset the encoding to attempt to apply to the arguments
+     * @return the arguments of `pid` as a String, suitable for writing to
+     *         stdout/stderr/etc if the encoding is correct.
+     * @throws IOException when the underlying library call fails
+     *                     (permission/nonexistance/malformed).
+     * @since 0.1
+     * @see <a href=
+     *      "https://gehrcke.de/2014/02/command-line-argument-binary-data/">Discussion
+     *      of process argument encoding</a>
+     */
     public static String asString(long pid, Charset charset) throws IOException {
         return asString(pid, charset, 0);
     }
 
+    /**
+     * Get the args of `pid` as a String, note the OS doesn't enforce an encoding on
+     * arguments to a process, so there is no guarantee that any particular Charset
+     * will be valid.
+     *
+     * @param pid     the process id (pid) of the process from which you want the
+     *                arguments
+     * @param charset the encoding to attempt to apply to the arguments
+     * @param skip    the number of leading arguments to skip over (leave off)
+     * @return the arguments of `pid` as a String, suitable for writing to
+     *         stdout/stderr/etc if the encoding is correct.
+     * @throws IOException when the underlying library call fails
+     *                     (permission/nonexistance/malformed).
+     * @since 0.1
+     * @see <a href=
+     *      "https://gehrcke.de/2014/02/command-line-argument-binary-data/">Discussion
+     *      of process argument encoding</a>
+     */
     public static String asString(long pid, Charset charset, long skip) throws IOException {
         return asString(pid, charset, skip, false);
     }
 
+    /**
+     * Get the args of `pid` as a String, note the OS doesn't enforce an encoding on
+     * arguments to a process, so there is no guarantee that any particular Charset
+     * will be valid.
+     *
+     * @param pid     the process id (pid) of the process from which you want the
+     *                arguments
+     * @param charset the encoding to attempt to apply to the arguments
+     * @param skip    the number of leading arguments to skip over (leave off)
+     * @param nuls    whether to replace the nul delimiters with spaces for human
+     * @return the arguments of `pid` as a String, suitable for writing to
+     *         stdout/stderr/etc if the encoding is correct.
+     * @throws IOException when the underlying library call fails
+     *                     (permission/nonexistance/malformed).
+     * @since 0.1
+     * @see <a href=
+     *      "https://gehrcke.de/2014/02/command-line-argument-binary-data/">Discussion
+     *      of process argument encoding</a>
+     */
     public static String asString(long pid, Charset charset, long skip, boolean nuls) throws IOException {
         if (skip < 0 || skip > ARG_MAX) {
             throw new IllegalArgumentException("skip outside valid range: 0-" + ARG_MAX);
@@ -72,6 +216,23 @@ public class Getargv {
         return new String(asBytes(pid, skip, nuls), charset);
     }
 
+    /**
+     * Get the args of `pid` as an array of Strings, note the OS doesn't enforce an
+     * encoding on arguments to a process, so there is no guarantee that any
+     * particular Charset will be valid.
+     *
+     * @param pid     the process id (pid) of the process from which you want the
+     *                arguments
+     * @param charset the encoding to attempt to apply to the arguments
+     * @return the arguments of `pid` as an array of Strings, suitable for
+     *         inspection if the encoding is correct.
+     * @throws IOException when the underlying library call fails
+     *                     (permission/nonexistance/malformed).
+     * @since 0.1
+     * @see <a href=
+     *      "https://gehrcke.de/2014/02/command-line-argument-binary-data/">Discussion
+     *      of process argument encoding</a>
+     */
     public static String[] asArrayOfStrings(long pid, Charset charset) throws IOException {
         if (pid < 0 || pid > PID_MAX) {
             throw new IllegalArgumentException("pid outside valid range: 0-" + PID_MAX);
--- src/main/java/cam/narzt/getargv/Main.java
+++ src/main/java/cam/narzt/getargv/Main.java
@@ -1,48 +1,56 @@
 package cam.narzt.getargv;
 
 import java.io.IOException;
-import cam.narzt.getargv.Getargv;
 
-public class Main {
+/**
+ * This class exists to hold a main method to test the Getargv class,
+ * this implementation of a getargv cli tool is really poor.
+ */
+public final class Main {
 
+    /**
+     * This is only here so you cannot instantiate this class
+     */
+    private Main() {
+        throw new AssertionError();
+    }
+
+    /**
+     * main method
+     *
+     * @param args the arguments passed in by the jvm
+     */
     public static void main(String[] args) {
         long pid = ProcessHandle.current().pid();
         boolean nuls = true;
         int skip = 0;
-        for (int i = 0; i<args.length; i++){
+        for (int i = 0; i < args.length; i++) {
             switch (args[i]) {
-            case "-s":
-                if (i+1 < args.length) {
-                    i++;
-                    skip = Integer.parseInt(args[i]);
-                } else {
-                    System.err.println("Unknown argument encountered");
-                    System.exit(-1);
-                }
-            case "-0":
-                nuls = false;
-            default:
-                if (args[i].charAt(0)=='-') {
-                    System.err.println("Unknown argument encountered");
-                    System.exit(-1);
-                } else if (i==args.length-1){
-                    pid = Integer.parseInt(args[i]);
-                }
+                case "-s":
+                    if (i + 1 < args.length) {
+                        i++;
+                        skip = Integer.parseInt(args[i]);
+                    } else {
+                        System.err.println("Unknown argument encountered");
+                        System.exit(-1);
+                    }
+                case "-0":
+                    nuls = false;
+                default:
+                    if (args[i].charAt(0) == '-') {
+                        System.err.println("Unknown argument encountered");
+                        System.exit(-1);
+                    } else if (i == args.length - 1) {
+                        pid = Integer.parseInt(args[i]);
+                    }
             }
         }
 
-        Getargv getargv = new Getargv();
         try {
-            // byte[][] array = getargv.asArray(pid);
-            // for(byte[] text: array){
-            //     System.out.write(text);
-            //     System.out.write('\0');
-            // }
-            byte[] text = getargv.asBytes(pid,skip,nuls);
+            byte[] text = Getargv.asBytes(pid, skip, nuls);
             System.out.write(text);
             System.out.flush();
         } catch (IOException e) {
-            // Oh no! Anyway...
         }
     }
 }
